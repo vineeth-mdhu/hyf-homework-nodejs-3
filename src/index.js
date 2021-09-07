fc8f7aa4fe6980944f373654b49572df2e129984
Vineeth Madhu <vineethnbr13@gmail.com>
2021-09-07 17:51:24 +0530

    add post and get users route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express= require('express');
 
 const app= express();
 
-const Users=[]
+let Users=[]
 
 app.listen(3000)
 
@@ -12,5 +12,15 @@ app.get('/',(req,res)=>{
 
 app.get('/users',(req,res)=>{
     res.send(Users);
+    res.end();
+});
+
+app.post('/users',(req,res)=>{
+    Users.push({id:Users.length()-1});
+    res.end();
+});
+
+app.get('/users/:id',(req,res)=>{
+    res.send(Users[req.params.id]);
     res.end();
 });
