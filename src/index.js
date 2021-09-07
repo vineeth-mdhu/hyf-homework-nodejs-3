6088719e4b4b1fad66b1ea60974c772a7b8e33b0
Vineeth Madhu <vineethnbr13@gmail.com>
2021-09-07 17:08:24 +0530

    add users route

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ const express= require('express');
 
 const app= express();
 
+const Users={}
+
 app.listen(3000)
 
 app.get('/',(req,res)=>{
     res.end('Hello World!');
 });
 
+app.get('/users',(req,res)=>{
+    res.json(Users);
+});
